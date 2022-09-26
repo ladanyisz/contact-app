2a7bb0b060492e9c9faae90444979577e8c27e0e
ladanyisz <92325224+szandil@users.noreply.github.com>
2022-09-26 19:07:02 +0200

    images, first components

---
+++ components/contacts.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { IContact } from "../models/Contact";
+import Contact from "./contact";
+
+const CONTACTS: IContact[] = [
+    {
+        name: 'Timothy Lewis',
+        image: '/images/Timothy.png',
+        phoneNum: '+36 01 234 5678'
+    },
+    {
+        name: 'Sarah Wright',
+        image: '/images/Sarah.png',
+        phoneNum: '+36 01 234 5678'
+    },
+    {
+        name: 'Lucy Jones',
+        image: '/images/Lucy.png',
+        phoneNum: '+36 01 234 5678'
+    }
+];
+
+const Contacts = () => {
+    return (
+        <Fragment>
+            <h1>Contacts</h1>
+            <ul>
+                {CONTACTS.map(contact => 
+                <Contact key={contact.name} contact={contact}/>)}
+            </ul>
+        </Fragment>
+    );
+}
+
+export default Contacts;
